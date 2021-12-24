5c71c866436c93e2796bfa0869063638d45fc5e4
Ashish Singh <ashishsingh9720@gmail.com>
2021-12-24 11:32:52 +0530

    added functions

--- src/component/Textutils.js
+++ src/component/Textutils.js
@@ -8,6 +8,21 @@ function Textutils(props) {
         const newtext = text.toUpperCase();
         setText(newtext);
     }
+    const handleDnClick= ()=>{
+        console.log("button was clicked");
+        const newtext = text.toLowerCase();
+        setText(newtext);
+    }
+    const handleRtClick= ()=>{
+        console.log("button was clicked");
+        setText("");
+    }
+    const handleReClick= ()=>{
+        console.log("button was clicked");
+        const newtext = text.trim();
+        setText(newtext);
+    }
+
     const handleOnChange= (e)=>
     {
         setText(e.target.value);
@@ -20,6 +35,9 @@ function Textutils(props) {
         <textarea className = "form-control" id="mybox" rows="8" value={text} onChange={handleOnChange}></textarea> 
         </div>
         <button className='btn btn-primary' onClick={handleUpClick}>Convert to upper Case</button>
+        <button className='btn btn-primary' onClick={handleDnClick}>Convert to Lower Case</button>
+        <button className='btn btn-primary' onClick={handleRtClick}>Remove text</button>
+        <button className='btn btn-primary' onClick={handleReClick}>Remove Extra Space</button>
     </div>
     )
 }
